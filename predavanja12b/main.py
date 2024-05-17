472d4e7e6b347fee110cf198cb64c201fe2e6031
tilenmarc <marct15@gmail.com>
2024-05-17 16:06:19 +0200

    Manjsi popravki.

--- predavanja12b/main.py
+++ predavanja12b/main.py
@@ -6,7 +6,7 @@ import shrani
 od, do = 1980, 1990
 
 
-# pridobi.pridobi(od, do)
+pridobi.pridobi(od, do)
 
 vsi_filmi = []
 for leto in range(od, do):
@@ -17,7 +17,7 @@ vsi_podatki = []
 for film in vsi_filmi:
     id = film[1]
     naslov = film[0]
-    # pridobi.pridobi_film(id)
+    pridobi.pridobi_film(id)
     podatki = izlusci.izlusci_film(id)
     podatki["naslov"] = naslov
     podatki["id"] = id
