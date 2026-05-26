06a75a70232a198b7a1155079575f92b9279e555
tilenmarc <marct15@gmail.com>
2026-05-26 11:16:14 +0200

    Predavanja 14.

--- predavanja11/b/pridobi_selenium.py
+++ predavanja11/b/pridobi_selenium.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-
 driver = webdriver.Chrome()
 
 driver.get("https://www.imdb.com/search/title/?title_type=feature")
